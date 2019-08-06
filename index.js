66b41ff866d60a3956a95b1821806d5ca6ac5a22
Himanshu <singh.himanshu3535@gmail.com>
2019-08-06 17:30:53 +0530

    Removing routes

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 console.log('Bootstraping...');
 
-const persons = require('./routes/persons');
 const express = require('express');
-const app = express();
 
-app.use(express.json());
-app.use('/api/persons', persons);
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+console.log('Express Loaded...' +express);
