0a3e6dca71176b459d774b4981c2a4d20915f8ac
Himanshu <singh.himanshu3535@gmail.com>
2019-08-06 17:11:29 +0530

    Adding express routing

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+console.log('Bootstraping...');
+
+const persons = require('./routes/persons');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use('/api/persons', persons);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
