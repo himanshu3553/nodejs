d5f6f2091c16508e66961bfe95110925b728ad18
Himanshu <singh.himanshu3535@gmail.com>
2019-08-08 13:13:06 +0530

    App Architecture

--- routes/persons.js
+++ routes/persons.js
@@ -1,14 +1,21 @@
+const { Person, validate } = require('../models/person');
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const persons = [
-  { id: 1, name: 'Himanshu' },  
-  { id: 2, name: 'Parul' },  
-  { id: 3, name: 'Sonal' },  
-];
+router.get('/', async (req, res) => {
+  const people = await Person.find().sort('name');
+  res.send(people);
+});
 
-router.get('/', (req, res) => {
-  res.send(persons);
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let person = new Person({ name: req.body.name });
+  person = await person.save();
+
+  res.send(person);
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -3,55 +3,16 @@ const persons = require('../routes/persons');
 const express = require('express');
 const app = express();
 
-
-app.use(express.json());
-app.use('/api/persons', persons);
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-// mongoose.connect('mongodb://127.0.0.1:27017/wm', { useNewUrlParser: true }.then(() => {
-//     console.log('Connected to Mongodb...');
-// }));
-
 // Database connection
-mongoose.connect('mongodb://localhost/wm')
+mongoose.connect('mongodb://localhost/auth')
     .then(() => {
         console.log('Connected to Mongodb...');
     }).catch(err => {
         console.log('Error in connecting with mongodb...', err);
     });
 
-// Schema 
-const personSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    date: { type: Date, default: Date.now },
-});
+app.use(express.json());
+app.use('/api/persons', persons);
 
-// Creating model using above schema
-const Person = mongoose.model('Person', personSchema);
-
-// async function to save documents into db
-async function createPerson() {
-    // making instance of the created model to be saved into database. 
-    const person = new Person({
-        name: "Himanshu Singh",
-        age: 29
-    });
-
-    // Saving person instance into db. 
-    // save() function is an async function so using await and async. 
-    const result = await person.save();
-    console.log(result);
-}
-
-async function getPersons() {
-    const persons = await Person.find({ name: 'Himanshu Singh' }).limit(10).sort({ name: 1 }).select({ name: 1 });
-    console.log(persons);
-}
-
-// createPerson();
-// getPersons();
-
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
