56e8b258209dcce53e5bf507c667cef571ca27fd
Himanshu <singh.himanshu3535@gmail.com>
2019-08-21 15:19:38 +0530

    Adding converter API

--- datasource/webservice-promise.js
+++ datasource/webservice-promise.js
@@ -2,26 +2,36 @@ const rp = require('request-promise');
 const express = require('express');
 const router = express.Router();
 
-function getData() {
+router.get('/', (req, res) => {
+
     // GET something from a JSON REST API
     var options = {
-        uri: 'http://httpbin.org/ip',
+        method: 'GET',
+        uri: 'https://currency-converter5.p.rapidapi.com/currency/convert',
+        headers: {
+            'x-rapidapi-host': 'currencyconverter.p.rapidapi.com',
+            'x-rapidapi-key': '22680d14f7msh18624d08fa2b566p1b0eaejsna79bfe16fc5b'
+        },
+        qs: {
+            'format': 'json',
+            'from': 'USD',
+            'to': 'AUD',
+            'amount': '10'
+        },
         json: true // Automatically parses the JSON string in the response
     };
-    
+
     rp(options)
         .then(function (value) {
-            console.log('API call success...' + value);
-            return value;
+            console.log('API call success...');
+            console.log(value);
+            res.send(value);
         })
         .catch(function (err) {
             console.log('API call failed...');
-        });
-}
-
-router.get('/', (req, res) => {
-    let data = getData();
-    res.send(data);
+            console.log(err);
+            res.send(err);
+        });    
 });
 
 module.exports = router;
